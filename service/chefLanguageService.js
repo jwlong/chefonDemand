6dd375a0088359f2c5ebd320e25e6a5866f15f65
dxfjyygy <dxfjyygy@sina.com>
2019-08-21 10:26:25 +0800

    setLanguage 修复

--- service/chefLanguageService.js
+++ service/chefLanguageService.js
@@ -2,6 +2,7 @@ import BaseService from './baseService.js'
 import {AutoWritedChefLanguage} from '../common/AutoWrite.js'
 import chefService from './chefService'
 import baseResult from "../model/baseResult";
+import utils from "../common/utils";
 
 @AutoWritedChefLanguage
 class ChefLanguageService extends BaseService{
@@ -29,6 +30,13 @@ class ChefLanguageService extends BaseService{
         if (!attr.language_list || (attr.language_list && attr.language_list.length ==0) ){
             throw baseResult.CHEF_LANG_LIST_EMPTY;
         }
+        attr.language_list.forEach(chefLang => {
+            if (chefLang) {
+                chefLang.chef_id = attr.chef_id;
+                chefLang.lang_code = chefLang.lang_code || chefLang.language_code;
+                utils.setCustomTransfer(chefLang,'create');
+            }
+        })
         return this.baseCreateBatch(attr.language_list);
     }
 }
